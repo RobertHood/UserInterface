55d9dbea0c47a2c1b9b4e295789425228f9bb945
Robbie <tranxuanthanhpbe@gmail.com>
2024-11-14 12:15:37 +0700

    commit

---
+++ logininterface.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class logininterface {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton loginButton;
+
+}
---
+++ userinterface.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class userinterface {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JButton registerButton;
+    private JButton loginButton;
+
+
+    public userinterface() {
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+    }
+}
